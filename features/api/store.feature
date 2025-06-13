61922e146cb4eaa64e8bc307952917fc52fa2894
nikosmakas <nikosmakas@protonmail.com>
2025-06-13 22:33:41 +0300

    refactor: remove dead code and unused files

--- features/api/store.feature
+++
@@ -1,6 +0,0 @@
-Feature: Store API Functionality
-  As a system
-  I want to interact with the store API endpoints
-  So that I can manage store data programmatically
-
-  # TODO: Add scenarios here 
--- step-definitions/api/store.steps.ts
+++
@@ -1,7 +0,0 @@
-import { Given, When, Then } from '@cucumber/cucumber';
-import { expect } from '@playwright/test';
-import { APIRequestContext } from '@playwright/test';
-
-let request: APIRequestContext;
-
-// TODO: Add step definitions here 
